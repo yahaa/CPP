aee1c4f9c16c2d19fe68ba72023f845c38f4ff58
yahaa <1477765176@qq.com>
2016-08-04 16:07:51 +0800

    新文件:       graph/LRJ11/duo5.cpp

---
+++ graph/LRJ11/duo5.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+#include <algorithm>
+#include <cstdio>
+#include <cstring>
+using namespace std;
+const int maxn=205;
+int dp[maxn][maxn];
+int main(){
+	int n,k;
+	while(cin>>n>>k){
+		memset(dp,0,sizeof(dp));
+		for(int i=1;i<=n;i++){
+			for(int j=1;j<=k;j++){
+				if(j>i)dp[i][j]=0;
+				else if(i==j)dp[i][j]=1;
+				else dp[i][j]=dp[i-j][j]+dp[i-1][j-1];
+			}
+		}
+		cout<<dp[n][k]<<endl;
+	}
+	
+}
