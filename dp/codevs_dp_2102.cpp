5e4e95a0006a1fb0ea0785f1172ef6c64e55970c
yahaa <1477765176@qq.com>
2016-09-05 22:21:43 +0800

    新文件:       dp/codevs_dp_2102.cpp

--- dp/codevs_dp_2102.cpp
+++ dp/codevs_dp_2102.cpp
@@ -24,48 +24,57 @@
 #define ROF(x,y) for(int x=y;x>=0;x--)
 #define LFOR(i,x,y) for(int i=x;i<=y;i++)
 #define ll long long
-#define INF 0X3FFFF
+#define INF 0X3F
 using namespace std;
-
-const int mxn=55;
-int dp[mxn][mxn];
-int tree[mxn][mxn];
-int w[mxn],n;
-
-void pre(int a,int b){
-	if(a>b)return;
-	int t=tree[a][b];
-	printf("%d ",t);
-	pre(a,t-1);
-	pre(t+1,b);
-}
+const int mxn=205;
+int dp[mxn][mxn],n,sum[mxn],a[mxn];
 
 int main(){
 	while(cin>>n){
-		MS(dp,0);
-		MS(tree,0);
+		MS(sum,0);
+		MS(dp,INF);
+		int t;
 		for(int i=1;i<=n;i++){
-			scanf("%d",&w[i]);
-			dp[i][i]=w[i];
-			tree[i][i]=i;
+			cin>>a[i];
+			a[i+n]=a[i];
+			dp[i][i]=0;
 		}
-		for(int i=n;i>=1;i--){
+		n*=2;
+		for(int i=1;i<=n;i++){
+			sum[i]=sum[i-1]+a[i];
+		}
+		for(int i=n-1;i>=1;i--){
 			for(int j=i+1;j<=n;j++){
-				for(int k=i;k<=j;k++){
-					int x=dp[i][k-1];
-					int y=dp[k+1][j];
-					x=(x==0)?1:x;
-					y=(y==0)?1:y;
-					if(dp[i][j]<x*y+w[k]){
-						dp[i][j]=x*y+w[k];
-						tree[i][j]=k;
-					}
+				for(int k=i;k<j;k++){
+					dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+sum[j]-sum[i-1]);
 				}
 			}
 		}
-		printf("%d\n",dp[1][n]);
-		pre(1,n);
-		cout<<endl;
+		int ans=10000000;
+		for(int i=1;i<=n/2-1;i++){
+			ans=min(dp[i][i+n/2-1],ans);
+		}
+		cout<<ans<<endl;
+		MS(dp,INF);
+		for(int i=1;i<=n;i++){
+			a[i]=-a[i];
+			sum[i]=-sum[i];
+			dp[i][i]=0;
+		}
+
+		for(int i=n-1;i>=1;i--){
+			for(int j=i+1;j<=n;j++){
+				for(int k=i;k<j;k++){
+					dp[i][j]=min(dp[i][j],dp[i][k]+dp[k+1][j]+sum[j]-sum[i-1]);
+				}
+			}
+		}
+
+		ans=10000000;
+		for(int i=1;i<=n/2-1;i++){
+			ans=min(dp[i][i+n/2-1],ans);
+		}
+		cout<<-ans<<endl;
 	}
 	return 0;
 }
