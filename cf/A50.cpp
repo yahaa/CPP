df93ba66194220a321a0bb84c70cdc1c23fe046a
yahaa <1477765176@qq.com>
2016-09-13 12:06:44 +0800

    cf 五道题

---
+++ cf/A50.cpp
@@ -0,0 +1,37 @@
+/*************************************************************************
+	> Author: yahaa
+	> Mail: yuanzihua0@gmail.com 
+ ************************************************************************/
+
+#include <iostream>
+#include <cstdio>
+#include <vector>
+#include <map>
+#include <queue>
+#include <stack>
+#include <algorithm>
+#include <cmath>
+#include <ctime>
+#include <string>
+#include <cstring>
+#include <set>
+#include <bitset>
+#include <functional>
+#include <iomanip>
+#include <cstdlib>
+#define FOR(x,y) for(int x=0;x<y;x++)
+#define MS(a,x) memset(a,x,sizeof(a))
+#define ROF(x,y) for(int x=y;x>=0;x--)
+#define LFOR(i,x,y) for(int i=x;i<=y;i++)
+#define ll long long
+#define INF 0X3FFFF
+using namespace std;
+
+int main(){
+	int n,m;
+	while(cin>>n>>m){
+		if(n%2)cout<<m*(n/2)+m/2<<endl;
+		else cout<<m*n/2<<endl;
+	}
+	return 0;
+}
